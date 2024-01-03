3cc1aba8d24502c46eadfbe99677f0787602186c
Fernando <fer3443@gmail.com>
2024-01-03 12:24:35 -0300

    carpeta components, service y config agregadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Login } from "./components/login/Login";
 
 function App() {
   return (
     <>
       <h1>Hola mundo!</h1>
+      <Login/>
     </>
   );
 }
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Login = () => {
+  return <section>
+		<h2>login inputs</h2>
+	</section>;
+};
