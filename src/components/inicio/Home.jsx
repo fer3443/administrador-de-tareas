926f948a49c4cdd74c723ae2fb45e86207f0b324
Fernando <fer3443@gmail.com>
2024-01-09 15:53:18 -0300

    barra de navegacion agregada

--- src/components/inicio/Home.jsx
+++ src/components/inicio/Home.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import "../inicio/Home.css";
-import { Link } from "react-router-dom";
 import { NavbarTask } from "../navbar/NavbarTask";
 export const Home = () => {
   return (
-    <header>
-      <h1>Bienvenido a su administrador de tareas!</h1>
-      <NavbarTask/>
-    </header>
+    <NavbarTask/>
   );
 };
--- src/components/navbar/NavbarTask.jsx
+++ src/components/navbar/NavbarTask.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import '../navbar/NavbarTask.css'
 export const NavbarTask = () => {
+  const [ showMenu, setShowMenu ] = useState(false)
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to={"/profile"}>Mi perfil</Link>
-        </li>
-        <li>
-          <Link to={"/home"}>Inicio</Link>
-        </li>
-      </ul>
-      <div className="btn-log-out">
-        <button>cerrar sesion</button>
-      </div>
-    </nav>
+    <header className="header">
+      <nav className="nav">
+      <span className="brand">Administrador de tareas</span>
+        <div className={showMenu ? "nav-menu showMenu" : "nav-menu"}>
+          <ul className="nav-list">
+            <li>
+              <Link to={"/home"} className="nav-item">
+                Inicio
+              </Link>
+            </li>
+            <li>
+              <Link to={"/profile"} className="nav-item">
+                Mi perfil
+              </Link>
+            </li>
+          <button className="btn-log-out">cerrar sesion</button>
+          </ul>
+          <FontAwesomeIcon icon={faCaretUp} className="nav-close" onClick={toggleMenu}/>
+        </div>
+        <FontAwesomeIcon icon={faCaretDown} className="nav-open"  onClick={toggleMenu}/>
+      </nav>
+    </header>
   );
 };
