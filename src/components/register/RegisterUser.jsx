2129fe2625822a79c4d4fe2a9d09401295ee4265
Fernando <fer3443@gmail.com>
2024-01-31 21:23:33 -0300

    formulario de registro creado

--- src/components/register/RegisterUser.jsx
+++ src/components/register/RegisterUser.jsx
@@ -1,5 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
+import { CreateUser } from "../../service/api";
+import { Link, useNavigate } from "react-router-dom";
+import { Loader } from "../loader/Loader";
+
+import "../register/RegisterUser.css";
 
 export const RegisterUser = () => {
-  return <div>RegisterUser</div>;
+  const [dataUser, setDataUser] = useState({
+    name: "",
+    userName: "",
+    password: "",
+    passConfirm: "",
+    avatar: "",
+  });
+  const [loading, setLoading] = useState(false);//agregar el loader
+  const navigate = useNavigate();
+  const validations = () => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDataUser((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    CreateUser({
+      name: dataUser.name,
+      userName: dataUser.userName,
+      password: dataUser.password,
+      avatar: dataUser.avatar,
+    })
+      .then((res) => {
+        setDataUser({
+          name: "",
+          userName: "",
+          password: "",
+          passConfirm: "",
+          avatar: "",
+        });
+        navigate("/");
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <section className="section-register section">
+      <Link to={"/"} className="link-back">
+        Login
+      </Link>
+      <div className="container-register container grid">
+        <div className="wrapper">
+          <h2 className="section-title">Registro</h2>
+          <hr />
+          <h3 className="section-subtitle">Ingrese los siguientes datos</h3>
+          <form action="">
+            <div className="box-input">
+              <input
+                type="text"
+                name="name"
+                value={dataUser.name}
+                onChange={handleChange}
+                placeholder="Nombre"
+                maxLength={30}
+                required
+              />
+            </div>
+            <div className="box-input">
+              <input
+                type="text"
+                name="userName"
+                value={dataUser.userName}
+                onChange={handleChange}
+                placeholder="Usuario"
+                maxLength={20}
+                required
+              />
+            </div>
+            <div className="box-input">
+              <input
+                type="password"
+                name="password"
+                value={dataUser.password}
+                onChange={handleChange}
+                placeholder="Contraseña"
+                required
+              />
+            </div>
+            <div className="box-input">
+              <input
+                type="password"
+                name="passConfirm"
+                value={dataUser.passConfirm}
+                onChange={handleChange}
+                placeholder="Confirmar contraseña"
+                required
+              />
+            </div>
+            <div className="box-input">
+              <input
+                type="text"
+                name="avatar"
+                value={dataUser.avatar}
+                onChange={handleChange}
+                placeholder="Url avatar"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn"
+              onClick={handleSubmit}
+            >registrarse</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/service/api.jsx
+++ src/service/api.jsx
@@ -23,3 +23,26 @@ export async function LoginUser({ userName, password }) {
     throw new Error("Hubo un error al iniciar sesion");
   }
 }
+
+export async function CreateUser({name, userName, password, avatar}){
+  try {
+    const body = JSON.stringify({
+      name,
+      userName,
+      password,
+      avatar
+    })
+    const response = await fetch(`${server.URL_CONECTION}/user/add`, {
+      method: "POST",
+      headers: {"content-type": "application/json"},
+      body: body
+    })
+    if(!response.ok){
+      const errorData = await response.json()
+      throw new Error(`Error al crear usuario ${response.status} - ${errorData.msg_error}`)
+    }
+    return await response.json()
+  } catch (error) {
+    throw new Error(`Error en el servidor ${error}`)
+  }
+}
