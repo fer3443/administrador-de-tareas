6fcb5c89dc03151a531f34ad817e8030bcbd3136
Fernando <fer3443@gmail.com>
2024-02-18 14:47:04 -0300

    UpdateUser agregado

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -45,3 +45,27 @@ export async function CreateUser({name, userName, password, avatar}){
     throw error
   }
 }
+export async function UpdateUser({ token, name, avatar, updatedAt}){
+  try {
+    const body = JSON.stringify({
+      name,
+      avatar
+    })
+    const response = await fetch(`${server.URL_LOCAL}/user/updateProfile`, {
+      method: 'PUT',
+      headers: {
+        "content-type": "application/json",
+        "authorization": `Bearer ${token}`
+      },
+      body
+    })
+    if(!response.ok){
+      const errorData = await response.json()
+      console.error("Error " + response.status)
+      throw new Error(errorData.msg_error)
+    }
+    return response.json()
+  } catch (error) {
+    throw error
+  }
+}
